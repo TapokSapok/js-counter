a866cc2e23ec0ee74b56ef68ac1e47879b672ef7
TapokSapok <119919066+TapokSapok@users.noreply.github.com>
2022-12-10 21:52:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const incCounter = document.querySelector('.counter');
+const btnClear = document.querySelector('.btn-counterClear');
+const btnAdd = document.querySelector('.btn-counterAdd');
+const btnReduce = document.querySelector('.btn-counterReduce');
+
+let counter = 0;
+
+function increment() {
+  counter += 1;
+  incCounter.innerText = counter;
+}
+
+function clear() {
+  counter = 0;
+  incCounter.innerText = counter;
+}
+
+function reduce() {
+  counter -= 1;
+  incCounter.innerText = counter
+}
+
+btnAdd.addEventListener('click', () => {
+  increment();
+});
+
+btnClear.addEventListener('click', () => {
+  clear()
+});
+
+btnReduce.addEventListener('click', () => {
+  reduce();
+});
+
+
